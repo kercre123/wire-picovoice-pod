ceaa0834bccd451de1758773c65d138c27fdbd7e
Wire <whyyoulittle333@gmail.com>
2022-07-18 17:54:08 -0500

    Now there is just one function for STT

--- chipper/go.mod
+++ chipper/go.mod
@@ -1,21 +1,50 @@
 module github.com/digital-dream-labs/chipper
 
-go 1.15
+go 1.18
 
 require (
-	github.com/Picovoice/cheetah/binding/go v1.0.3 // indirect
-	github.com/Picovoice/leopard/binding/go v1.0.3 // indirect
-	github.com/Picovoice/rhino/binding/go/v2 v2.1.5 // indirect
-	github.com/asticode/go-asticoqui v0.1.0 // indirect
+	github.com/Picovoice/cheetah/binding/go v1.0.3
+	github.com/Picovoice/leopard/binding/go v1.0.3
+	github.com/Picovoice/rhino/binding/go/v2 v2.1.5
 	github.com/digital-dream-labs/api v0.0.0-20201230194801-ee4f8f160098
 	github.com/digital-dream-labs/hugh v0.0.0-20210210154335-f4159b9fcd5f
-	github.com/digital-dream-labs/opus-go v0.0.0-20201230195736-934a8a9e0a1e // indirect
-	github.com/go-audio/audio v1.0.0 // indirect
-	github.com/go-audio/wav v1.1.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/soundhound/houndify-sdk-go v0.3.4 // indirect
-	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b // indirect
+	github.com/digital-dream-labs/opus-go v0.0.0-20201230195736-934a8a9e0a1e
+	github.com/go-audio/audio v1.0.0
+	github.com/go-audio/wav v1.1.0
+	github.com/pkg/errors v0.9.1
+	github.com/soundhound/houndify-sdk-go v0.3.4
 	google.golang.org/grpc v1.32.0
-	gopkg.in/pipe.v2 v2.0.0-20140414041502-3c2ca4d52544 // indirect
+)
+
+require (
+	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
+	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-audio/riff v1.0.0 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/grd/ogg v0.0.0-20130623210630-0dae53159b70 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.0.0-beta.5 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/mitchellh/mapstructure v1.3.2 // indirect
+	github.com/pelletier/go-toml v1.8.0 // indirect
+	github.com/sirupsen/logrus v1.7.0 // indirect
+	github.com/spf13/afero v1.2.2 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20200923140941-5646d36feee1 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
+	gopkg.in/hraban/opus.v2 v2.0.0-20201025103112-d779bb1cc5a2 // indirect
+	gopkg.in/ini.v1 v1.57.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200605160147-a5ece683394c // indirect
 )
--- chipper/pkg/voice_processors/wirepod/knowledgegraph.go
+++ chipper/pkg/voice_processors/wirepod/knowledgegraph.go
@@ -3,26 +3,18 @@ package wirepod
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"log"
 	"os"
 	"strconv"
 	"strings"
-	"time"
 
-	cheetah "github.com/Picovoice/cheetah/binding/go"
-	leopard "github.com/Picovoice/leopard/binding/go"
 	pb "github.com/digital-dream-labs/api/go/chipperpb"
 	"github.com/digital-dream-labs/chipper/pkg/vtt"
-	opus "github.com/digital-dream-labs/opus-go/opus"
 	"github.com/pkg/errors"
 	"github.com/soundhound/houndify-sdk-go"
 )
 
 var hclient houndify.Client
 var houndEnable bool = true
-
-var disableLiveTranscriptionKG bool = false
 
 func ParseSpokenResponse(serverResponseJSON string) (string, error) {
 	result := make(map[string]interface{})
@@ -66,8 +58,6 @@ func InitHoundify() {
 var NoResult string = "NoResultCommand"
 var NoResultSpoken string
 
-var botNumKG int = 0
-
 func knowledgeAPI(spokenText string, req *vtt.KnowledgeGraphRequest) string {
 	if houndEnable == true {
 		hReq := houndify.TextRequest{
@@ -98,52 +88,15 @@ func knowledgeAPI(spokenText string, req *vtt.KnowledgeGraphRequest) string {
 }
 
 func (s *Server) ProcessKnowledgeGraph(req *vtt.KnowledgeGraphRequest) (*vtt.KnowledgeGraphResponse, error) {
-	var voiceTimer int = 0
-	var transcription1 string = ""
-	var transcription2 string = ""
-	var transcribedText string
-	var isOpus bool
-	var micData []int16
-	var micDataRhino [][]int16
-	var die bool = false
-	var doSTT bool = true
-	var sayStarting = true
-	var leopardSTT leopard.Leopard
-	var cheetahSTT cheetah.Cheetah
-	var numInRange int = 0
-	var oldDataLength int = 0
-	if os.Getenv("DEBUG_LOGGING") != "true" && os.Getenv("DEBUG_LOGGING") != "false" {
-		fmt.Println("No valid value for DEBUG_LOGGING, setting to true")
-		debugLogging = true
-	} else {
-		if os.Getenv("DEBUG_LOGGING") == "true" {
-			debugLogging = true
-		} else {
-			debugLogging = false
-		}
-	}
-	botNumKG = botNumKG + 1
-	botNum = botNum + 1
-	justThisBotNum := botNumKG
-	justThisBotNumReg := botNum
-	if botNum > 1 {
-		disableLiveTranscription = true
-	} else {
-		disableLiveTranscription = false
-	}
-	if os.Getenv("DISABLE_LIVE_TRANSCRIPTION") == "true" {
-		disableLiveTranscription = true
-	}
-	if botNum > picovoiceInstances {
-		fmt.Println("Too many bots are connected, sending error to bot " + strconv.Itoa(justThisBotNum))
-		NoResultSpoken = "Too many bots are connected, please try again later."
+	if picovoiceModeOS == "OnlyRhino" {
+		fmt.Println("Knowledge Graph does not work in Rhino mode.")
+		NoResultSpoken = "Knowledge Graph does not work in Rhino mode."
 		kg := pb.KnowledgeGraphResponse{
 			Session:     req.Session,
 			DeviceId:    req.Device,
 			CommandType: NoResult,
 			SpokenText:  NoResultSpoken,
 		}
-		botNumKG = botNumKG - 1
 		botNum = botNum - 1
 		if err := req.Stream.Send(&kg); err != nil {
 			return nil, err
@@ -152,213 +105,27 @@ func (s *Server) ProcessKnowledgeGraph(req *vtt.KnowledgeGraphRequest) (*vtt.Kno
 			Intent: &kg,
 		}, nil
 		return nil, nil
-	} else {
-		if picovoiceModeOS == "OnlyLeopard" || picovoiceModeOS == "LeopardAndRhino" {
-			leopardSTT = leopardSTTArray[botNum-1]
-		} else if picovoiceModeOS == "OnlyCheetah" {
-			cheetahSTT = cheetahSTTArray[botNum-1]
-		} else {
-			fmt.Println("Q&A is not enabled due to OnlyRhino mode.")
-			kg := pb.KnowledgeGraphResponse{
-				Session:     req.Session,
-				DeviceId:    req.Device,
-				CommandType: NoResult,
-				SpokenText:  "Q and A is not available due to the Only Rhino environment variable.",
-			}
-			botNumKG = botNumKG - 1
-			botNum = botNum - 1
-			if err := req.Stream.Send(&kg); err != nil {
-				return nil, err
-			}
-			return &vtt.KnowledgeGraphResponse{
-				Intent: &kg,
-			}, nil
-			return nil, nil
-		}
 	}
-	if debugLogging == true {
-		fmt.Println("(KG) Bot " + strconv.Itoa(botNumKG) + " ESN: " + req.Device)
-		fmt.Println("(KG) Bot " + strconv.Itoa(botNumKG) + " Session: " + req.Session)
-		fmt.Println("(KG) Bot " + strconv.Itoa(botNumKG) + " Language: " + req.LangString)
-		fmt.Println("(KG) Stream " + strconv.Itoa(botNumKG) + " opened.")
+	transcribedText, transcribedSlots, isRhino, justThisBotNum, isOpus, err := sttHandler(req, true)
+	if transcribedSlots == nil && isRhino == false && isOpus == false {
+		// don't do anything
 	}
-	data := []byte{}
-	data = append(data, req.FirstReq.InputAudio...)
-	if len(data) > 0 {
-		if data[0] == 0x4f {
-			isOpus = true
-			if debugLogging == true {
-				fmt.Println("(KG) Bot " + strconv.Itoa(botNumKG) + " Stream Type: Opus")
-			}
-		} else {
-			isOpus = false
-			if debugLogging == true {
-				fmt.Println("(KG) Bot " + strconv.Itoa(botNumKG) + " Stream Type: PCM")
-			}
+	if err != nil {
+		fmt.Println(err)
+		NoResultSpoken = err.Error()
+		kg := pb.KnowledgeGraphResponse{
+			Session:     req.Session,
+			DeviceId:    req.Device,
+			CommandType: NoResult,
+			SpokenText:  NoResultSpoken,
 		}
-	}
-	stream := opus.OggStream{}
-	go func() {
-		if isOpus == true {
-			time.Sleep(time.Millisecond * 500)
-		} else {
-			time.Sleep(time.Millisecond * 1100)
+		if err := req.Stream.Send(&kg); err != nil {
+			return nil, err
 		}
-		for voiceTimer < 7 {
-			voiceTimer = voiceTimer + 1
-			time.Sleep(time.Millisecond * 750)
-		}
-	}()
-	go func() {
-		if botNum > 1 {
-			disableLiveTranscriptionKG = true
-		}
-		if picovoiceModeOS != "OnlyCheetah" {
-			for doSTT == true {
-				if micData != nil && die == false && voiceTimer > 0 {
-					if sayStarting == true {
-						if debugLogging == true {
-							fmt.Printf("(KG) Transcribing stream %d...\n", justThisBotNum)
-						}
-						sayStarting = false
-					}
-					processOneData := micData
-					transcription1Raw, err := leopardSTT.Process(processOneData)
-					if err != nil {
-						log.Println(err)
-					}
-					transcription1 = strings.ToLower(transcription1Raw)
-					if debugLogging == true {
-						if disableLiveTranscriptionKG == false {
-							fmt.Printf("\r(KG) Bot " + strconv.Itoa(justThisBotNum) + " Transcription: " + transcription1)
-						}
-					}
-					if transcription1 != "" && transcription2 != "" && transcription1 == transcription2 {
-						transcribedText = transcription1
-						if debugLogging == true {
-							if disableLiveTranscriptionKG == false {
-								fmt.Printf("\n")
-							} else {
-								fmt.Println("\r(KG) Bot " + strconv.Itoa(justThisBotNum) + " Transcription: " + transcribedText)
-							}
-						}
-						die = true
-						break
-					} else if voiceTimer == 7 {
-						transcribedText = transcription2
-						if debugLogging == true {
-							if disableLiveTranscriptionKG == false {
-								fmt.Printf("\n")
-							} else {
-								fmt.Println("\r(KG) Bot " + strconv.Itoa(justThisBotNum) + " Transcription: " + transcribedText)
-							}
-						}
-						die = true
-						break
-					}
-					time.Sleep(time.Millisecond * 150)
-					processTwoData := micData
-					transcription2Raw, err := leopardSTT.Process(processTwoData)
-					if err != nil {
-						log.Println(err)
-					}
-					transcription2 = strings.ToLower(transcription2Raw)
-					if debugLogging == true {
-						if disableLiveTranscriptionKG == false {
-							fmt.Printf("\r(KG) Bot " + strconv.Itoa(justThisBotNum) + " Transcription: " + transcription2)
-						}
-					}
-					if transcription1 != "" && transcription2 != "" && transcription1 == transcription2 {
-						transcribedText = transcription1
-						if debugLogging == true {
-							if disableLiveTranscriptionKG == false {
-								fmt.Printf("\n")
-							} else {
-								fmt.Println("\r(KG) Bot " + strconv.Itoa(justThisBotNum) + " Transcription: " + transcribedText)
-							}
-						}
-						die = true
-						break
-					} else if voiceTimer == 7 {
-						transcribedText = transcription2
-						if debugLogging == true {
-							if disableLiveTranscriptionKG == false {
-								fmt.Printf("\n")
-							} else {
-								fmt.Println("\r(KG) Bot " + strconv.Itoa(justThisBotNum) + " Transcription: " + transcribedText)
-							}
-						}
-						die = true
-						break
-					}
-					time.Sleep(time.Millisecond * 150)
-				}
-			}
-		}
-	}()
-	for {
-		chunk, err := req.Stream.Recv()
-		if err != nil {
-			if err == io.EOF {
-				transcribedText = ""
-				break
-			}
-		}
-
-		data = append(data, chunk.InputAudio...)
-		micData = bytesToIntLeopard(stream, data, die, isOpus)
-		if picovoiceModeOS == "OnlyCheetah" {
-			// returns [][]int16, 512 framesize
-			micDataRhino = bytesToIntRhino(stream, data, die, isOpus)
-			numInRange = 0
-			for _, sample := range micDataRhino {
-				if numInRange >= oldDataLength {
-					if sayStarting == true {
-						if debugLogging == true {
-							fmt.Printf("(KG) Transcribing stream %d...\n", justThisBotNum)
-						}
-						sayStarting = false
-					}
-					partialTranscript, isEndpoint, err := cheetahSTT.Process(sample)
-					if partialTranscript != "" {
-						transcribedText = strings.ToLower(transcribedText + strings.TrimSpace(partialTranscript) + " ")
-						if debugLogging == true && disableLiveTranscription == false {
-							fmt.Printf("\r(KG) Bot " + strconv.Itoa(justThisBotNum) + " Transcription: " + transcribedText)
-						}
-					}
-					if isEndpoint {
-						finalTranscript, err := cheetahSTT.Flush()
-						transcribedText = strings.TrimSpace(strings.ToLower(transcribedText + finalTranscript))
-						if debugLogging == true {
-							if disableLiveTranscription == false {
-								fmt.Printf("\r(KG) Bot " + strconv.Itoa(justThisBotNum) + " Final Transcription: " + transcribedText + "\n")
-							} else {
-								fmt.Println("(KG) Bot " + strconv.Itoa(justThisBotNum) + " Final Transcription: " + transcribedText)
-							}
-						}
-						die = true
-						break
-						if err != nil {
-							fmt.Println("Error: " + err.Error())
-						}
-					}
-					if err != nil {
-						fmt.Println("Error: " + err.Error())
-						break
-					}
-					numInRange = numInRange + 1
-				} else {
-					numInRange = numInRange + 1
-				}
-			}
-			oldDataLength = len(micDataRhino)
-			if die == true {
-				break
-			}
-		}
-		if die == true {
-			break
-		}
+		return &vtt.KnowledgeGraphResponse{
+			Intent: &kg,
+		}, nil
+		return nil, nil
 	}
 	NoResultSpoken = knowledgeAPI(transcribedText, req)
 	kg := pb.KnowledgeGraphResponse{
@@ -367,10 +134,8 @@ func (s *Server) ProcessKnowledgeGraph(req *vtt.KnowledgeGraphRequest) (*vtt.Kno
 		CommandType: NoResult,
 		SpokenText:  NoResultSpoken,
 	}
-	botNumKG = botNumKG - 1
-	botNum = botNum - 1
 	if debugLogging == true {
-		fmt.Println("Bot " + strconv.Itoa(justThisBotNumReg) + " (KG Bot " + strconv.Itoa(justThisBotNum) + ") request served.")
+		fmt.Println("(KG) Bot " + strconv.Itoa(justThisBotNum) + " request served.")
 	}
 	if err := req.Stream.Send(&kg); err != nil {
 		return nil, err
